006b07b5534af2aba171f0aec29c5f76371c6bd4
gugohh <oohhgug@gmail.com>
2022-03-19 20:09:56 +0100

    added Error component for error maneging

---
+++ client/src/componentes/Error/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <section className="error">
+      <h2 className="error-title_error">Error... page not found</h2>
+    </section>
+  );
+};
+
+export default Error;
