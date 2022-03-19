d240478e1f334528b149eceb78fd27e52ab14252
gugohh <oohhgug@gmail.com>
2022-03-19 20:22:43 +0100

    Changed NavBar component

---
+++ client/src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+    <div className="container">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Offer</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
